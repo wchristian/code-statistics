6d3e4d78b733a1022e51b1efaf88f633c5d3993e
Christian Walde <walde.christian@googlemail.com>
2010-08-16 13:22:40 +0200

    making better use of Moose facilities in File.pm now

--- lib/Code/Statistics/File.pm
+++ lib/Code/Statistics/File.pm
@@ -13,41 +13,35 @@ has path => (
     required => 1,
 );
 
-has targets => (
-    isa => 'ArrayRef',
-);
-
-has metrics => (
-    isa => 'ArrayRef',
-);
-
 has collector => (
     isa => 'Code::Statistics::Collector',
     required => 1,
 );
 
-
-sub ppi {
-    return PPI::Document->new( $_[0]->path );
-}
+has ppi => (
+    isa => 'PPI::Document',
+    lazy => 1,
+    default => sub {
+        PPI::Document->new( $_[0]->path );
+    },
+);
 
 sub analyze {
     my ( $self ) = @_;
 
-    my $ppi = $self->ppi;
-    $self->_process_target_class( $ppi, $_ ) for @{ $self->targets };
+    $self->_process_target_class( $_ ) for @{ $self->collector->targets };
     $self->collector->progress_bar->increment;
 
     return $self;
 }
 
 sub _process_target_class {
-    my ( $self, $ppi, $target_type ) = @_;
+    my ( $self, $target_type ) = @_;
 
     my ( $ppi_class, @supported_metrics ) = $self->_get_target_class_data( $target_type );
     return if !@supported_metrics;
 
-    my $targets = $ppi->find( $ppi_class );
+    my $targets = $self->ppi->find( $ppi_class );
     return if !$targets;
 
     my @measurements = map _measure_target( $_, @supported_metrics ), @{$targets};
@@ -60,7 +54,7 @@ sub _get_target_class_data {
     my ( $self, $target_type ) = @_;
 
     my $package           = "Code::Statistics::Target::$target_type";
-    my @supported_metrics = grep { $package->supports( $_ ) } @{ $self->metrics };
+    my @supported_metrics = grep { $package->supports( $_ ) } @{ $self->collector->metrics };
     my $ppi_class         = $package->class;
 
     return ( $ppi_class, @supported_metrics );
