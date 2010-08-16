c888cb85a11d89ef2dbc24deef6e49ac04807552
Christian Walde <walde.christian@googlemail.com>
2010-08-16 11:19:26 +0200

    made argument handling in collect command a bit more explicit and easier to read

--- lib/Code/Statistics/App/Command/collect.pm
+++ lib/Code/Statistics/App/Command/collect.pm
@@ -7,13 +7,19 @@ use Code::Statistics::App -command;
 sub abstract { 'gather measurements on targets and write them to disk' }
 
 sub opt_spec {
-    [ 'dirs=s' => 'the directories in which to to search for perl code files', { default => '.' } ],;
+    return (
+        [ 'dirs=s' => 'the directories in which to to search for perl code files', { default => '.' } ],
+    );
 }
 
 sub execute {
     my ( $self, $opt, $arg ) = @_;
 
-    return $self->cstat( dirs => $opt->dirs )->collect;
+    my %args = (
+        dirs => $opt->dirs,
+    );
+
+    return $self->cstat( %args )->collect;
 }
 
 1;
