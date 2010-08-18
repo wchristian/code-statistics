517024b79b9e10a99f2c50caeff29c9322a16f03
Christian Walde <walde.christian@googlemail.com>
2010-08-18 18:08:00 +0200

    split regression sub into own method

--- t/lib/ShellTest.pm
+++ t/lib/ShellTest.pm
@@ -14,12 +14,17 @@ sub basic_collect : Test {
     local @ARGV = qw( collect --no_dump --relative_paths --foreign_paths=Unix );
 
     ok_regression(
-                  sub {
-                    my $result = Code::Statistics::App->run;
-                    return $result;
-                    }, "data/json/basic_collect.json");
+        sub { $self->run_codestat_shell_app },
+        "data/json/basic_collect.json",
+        'returned string matches expected output'
+    );
 
     return;
+}
+
+sub run_codestat_shell_app {
+    my $result = Code::Statistics::App->run;
+    return $result;
 }
 
 1;
