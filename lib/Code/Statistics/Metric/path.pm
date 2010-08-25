1b937c998899970dc4b056a0732db78091217a87
Christian Walde <walde.christian@googlemail.com>
2010-08-25 10:55:27 +0200

    added a placeholder metric for the path

---
+++ lib/Code/Statistics/Metric/path.pm
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+
+package Code::Statistics::Metric::path;
+
+# ABSTRACT: measures the starting column of a target
+
+use Moose;
+extends 'Code::Statistics::Metric';
+
+=head2 incompatible_with
+    Returns true if the given target is explicitly not supported by this metric.
+
+    Returns false for this class, since it is never measured and just serves as a placeholder for the path column.
+=cut
+
+sub incompatible_with {
+    my ( $class, $target ) = @_;
+    return 1;
+}
+
+=head2 is_insignificant
+    Returns true if the metric is considered statistically insignificant.
+
+    Returns false for this class, since it only identifies the location of a
+    target.
+=cut
+
+sub is_insignificant {
+    my ( $class ) = @_;
+    return 1;
+}
+
+1;
